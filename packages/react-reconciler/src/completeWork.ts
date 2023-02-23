dfcf403867816350b1bd15aac23a1ea09bc31b48
lmlss <18693435820@163.com>
2023-02-23 13:21:40 +0800

    feat: function-component

--- packages/react-reconciler/src/completeWork.ts
+++ packages/react-reconciler/src/completeWork.ts
@@ -6,7 +6,12 @@ import {
 } from 'react-dom/src/hostConfig';
 import { FiberNode } from './fiber';
 import { NoFlags } from './fiberFlags';
-import { HostComponent, HostRoot, HostText } from './workTags';
+import {
+	FunctionComponent,
+	HostComponent,
+	HostRoot,
+	HostText
+} from './workTags';
 
 export const completeWork = (wip: FiberNode) => {
 	// 递归中的归
@@ -39,6 +44,9 @@ export const completeWork = (wip: FiberNode) => {
 			bubbleProperties(wip);
 			return null;
 		case HostRoot:
+			bubbleProperties(wip);
+			return null;
+		case FunctionComponent:
 			bubbleProperties(wip);
 			return null;
 		default:
